b9051f798d11b3fe179831e1ef5bea6eac2fb2c9
KaoutarLanjri <lanjrikaoutar@icloud.com>
2021-07-26 15:27:39 +0200

    update

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import spacy
 import pandas as pd
-import plotly_express as px
 import plotly.graph_objects as go
 
 # containers
 header = st.beta_container()
 dataset = st.beta_container()
+features = st.beta_container()
 
 with header:
     st.title("NLP for Quantified-Self Forum")
@@ -17,9 +17,16 @@ with dataset:
     st.header('QS forum dataset')
     st.text('The dataset was extracted from QS website using Python to parse the JSON files')
 
-    qs_data = pd.read_csv('datasets/global_df')
+    qs_data = pd.read_csv('datasets/global_df.csv')
     st.write(qs_data.head())
-#
+
+    st.subheader('Distribution of Monthly created docs')
+    docs_list = pd.DataFrame(qs_data['date_month'].value_counts())
+    st.bar_chart(docs_list)
+
+with features:
+    st.header('Features created')
+
 
 def main():
     """NLP APP"""
