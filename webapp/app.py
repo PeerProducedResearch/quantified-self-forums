120af723fef408f50a3a86bb08dbe2feef408cf7
KaoutarLanjri <lanjrikaoutar@icloud.com>
2021-08-02 09:52:35 +0200

    app_update

--- webapp/app.py
+++ webapp/app.py
@@ -121,110 +121,24 @@ df = df.groupby(['topic_id'], as_index=False).agg({('no_sw_LDA_text'): ' '.join}
 df['token_NN_text'] = df.no_sw_LDA_text.apply(lambda x: tokenize_text(x))
 
 
-
 # FUNCTIONS PREPROCESSING FOR TOPIC MODELLING
+dictionary = corpora.Dictionary(df.token_NN_text)
 
-# Example for detecting bigrams
-bigram_measures = nltk.collocations.BigramAssocMeasures()
+# filter words appear less than 15 docs & more than 0.5 documents & keep only 100k most frequent words
+dictionary.filter_extremes(no_below=20, no_above=0.5)
+# document to 'bow' BAG OF WORDS
+corpus = [dictionary.doc2bow(doc) for doc in df.token_NN_text]
 
-finder =nltk.collocations.BigramCollocationFinder\
-.from_documents([comment.split() for comment in\
-                 df.no_sw_LDA_text])
-
-
-# Filter only those that occur at least 50 times
-finder.apply_freq_filter(50)
-bigram_scores = finder.score_ngrams(bigram_measures.pmi)
-
-
-trigram_measures = nltk.collocations.TrigramAssocMeasures()
-finder = nltk.collocations.TrigramCollocationFinder.from_documents([comment.split() for comment in df.no_sw_LDA_text])
-# Filter only those that occur at least 50 times
-finder.apply_freq_filter(50)
-trigram_scores = finder.score_ngrams(trigram_measures.pmi)
-
-bigram_pmi = pd.DataFrame(bigram_scores)
-bigram_pmi.columns = ['bigram', 'pmi']
-bigram_pmi.sort_values(by='pmi', axis=0, ascending=False, inplace=True)
-
-trigram_pmi = pd.DataFrame(trigram_scores)
-trigram_pmi.columns = ['trigram', 'pmi']
-trigram_pmi.sort_values(by='pmi', axis=0, ascending=False, inplace = True)
-
-# Filter for bigrams with only noun-type structures
-@st.cache
-def bigram_filter(bigram):
-    tag = nltk.pos_tag(bigram)
-    if tag[0][1] not in ['JJ', 'NN'] and tag[1][1] not in ['NN']:
-        return False
-    if 'n' in bigram or 't' in bigram:
-        return False
-    if 'PRON' in bigram:
-        return False
-    return True
-
-             # Filter for trigrams with only noun-type structures
-@st.cache
-def trigram_filter(trigram):
-    tag = nltk.pos_tag(trigram)
-    if tag[0][1] not in ['JJ', 'NN'] and tag[1][1] not in ['JJ','NN']:
-        return False
-    if 'n' in trigram or 't' in trigram:
-         return False
-    if 'PRON' in trigram:
-        return False
-    return True
-
- # choose top 500 ngrams in this case ranked by PMI that have noun like structures
-filtered_bigram = bigram_pmi[bigram_pmi.apply(lambda bigram:\
-                                              bigram_filter(bigram['bigram'])\
-                                              and bigram.pmi > 5, axis = 1)][:500]
-
-filtered_trigram = trigram_pmi[trigram_pmi.apply(lambda trigram: \
-                                                 trigram_filter(trigram['trigram'])\
-                                                 and trigram.pmi > 5, axis = 1)][:500]
-
-
-bigrams = [' '.join(x) for x in filtered_bigram.bigram.values if len(x[0]) > 2 or len(x[1]) > 2]
-trigrams = [' '.join(x) for x in filtered_trigram.trigram.values if len(x[0]) > 2 or len(x[1]) > 2 and len(x[2]) > 2]
-
-      # Concatenate n-grams
-@st.cache
-def replace_ngram(x):
-    for gram in trigrams:
-        x = x.replace(gram, '_'.join(gram.split()))
-    for gram in bigrams:
-        x = x.replace(gram, '_'.join(gram.split()))
-    return x
-
-reviews_w_ngrams = df.copy()
-reviews_w_ngrams.no_sw_LDA_text = reviews_w_ngrams.no_sw_LDA_text.map(lambda x: replace_ngram(x))
-
-# tokenize reviews + remove stop words + remove names + remove words with less than 2 characters
-reviews_w_ngrams = reviews_w_ngrams.no_sw_LDA_text.map(lambda x: [word for word in x.split()\
-                                                 if  len(word) > 2])
-
-# Filter for only nouns
-@st.cache
-def noun_only(x):
-    pos_comment = nltk.pos_tag(x)
-    filtered = [word[0] for word in pos_comment if word[1] in ['NN']]
-    return filtered
-
-final_posts = reviews_w_ngrams.map(noun_only)
-
-# TOPIC MODELLING
-dictionary = corpora.Dictionary(final_posts)
-doc_term_matrix = [dictionary.doc2bow(doc) for doc in final_posts]
-
+# MODEL
 Lda = gensim.models.ldamodel.LdaModel
-ldamodel = Lda(doc_term_matrix, num_topics=10, id2word = dictionary, passes=40,\
-               iterations=200,  chunksize = 10000, eval_every = None, random_state=0)
+
+ldamodel = Lda(corpus, num_topics=11, id2word = dictionary, passes=10,\
+               iterations=100,  chunksize =1000, random_state=100, eval_every = None, alpha='auto', eta='auto')
 
 from pyLDAvis import gensim
 from gensim.models import LdaModel, HdpModel
 
-vis_data1 = pyLDAvis.gensim.prepare(ldamodel, doc_term_matrix, dictionary)
+vis_data1 = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
 pyLDAvis.display(vis_data1)
 
 html_string1 = pyLDAvis.prepared_data_to_html(vis_data1)
